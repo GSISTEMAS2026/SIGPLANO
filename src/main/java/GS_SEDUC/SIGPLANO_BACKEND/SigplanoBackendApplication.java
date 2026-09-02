c55297306418db1ebd089706ddb2a13d108d11a2
Weslley Reis Silva <weslly1997@gmail.com>
2026-09-02 09:39:40 -0300

    Starter Projeto SigPlano.

---
+++ src/main/java/GS_SEDUC/SIGPLANO_BACKEND/SigplanoBackendApplication.java
@@ -0,0 +1,13 @@
+package GS_SEDUC.SIGPLANO_BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigplanoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigplanoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GS_SEDUC/SIGPLANO_BACKEND/SigplanoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package GS_SEDUC.SIGPLANO_BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigplanoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
